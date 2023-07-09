c6d8ac15e0f033e3ac6566e7df7659cc1c3a6a1c
joaobassiDenver <137333862+joaobassiDenver@users.noreply.github.com>
2023-07-09 18:49:27 -0300

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
-import tabula as tb
-import openpyxl
+import tabula
+import io
+import tempfile
+import re
 
 st.title('Geradora Carteira em PDF')
 
@@ -9,63 +11,56 @@ st.image('https://th.bing.com/th/id/R.592a9b01eb077958f53aa385dc40f1d5?rik=ESxDz
 
 file = st.file_uploader(
     'Importe a carteira em PDF',
-    type = 'pdf'
+    type='pdf'
 )
 
-pdf = st.file_uploader(
-    'Selecione o arquivo csv',
-    type = 'csv'
-)
+if file is not None:
+    # Convert PDF to CSV
+    with tempfile.NamedTemporaryFile(suffix='.csv', delete=False) as temp_file:
+        temp_file_path = temp_file.name
+        tabula.convert_into(file, temp_file_path, output_format='csv', pages='all')
 
-tb.convert_into(file, "extrato.csv", output_format="csv", pages= "all")
+    # Read CSV into DataFrame
+    pdf = pd.read_csv(temp_file_path, encoding='latin1', decimal=',')
 
-def download_excel(file):
-    output = io.BytesIO()
-    df.to_excel(output, index=False, engine='openpyxl')
-    output.seek(0)
-    return output
+    conta = pdf.iloc[1][1]
 
-download_link = download_excel(file)
-st.markdown(download_link, unsafe_allow_html=True)
+    conta = re.findall(r'\d+', conta)
+    n_conta = f'{conta[1]}-{conta[2]}'
 
-st.download_button(f'Baixar carteira em CSV', data=download_link, file_name='Carteira.csv', 
-mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    pdf.iloc[:, 2] = pdf.iloc[:, 2].fillna(' ')
 
-pdf = pd.read_csv('extrato.csv', encoding='latin1',decimal = ',')
+    pdf.iloc[:, 1] = pdf.iloc[:, 1] + ' ' + pdf.iloc[:, 2]
 
-conta = pdf.iloc[1][1]
+    pdf = pdf.drop(pdf.columns[2], axis=1)
+    pdf = pdf.drop(pdf.columns[4], axis=1)
 
-conta = re.findall(r'\d+', conta)
-n_conta = f'{conta[1]}-{conta[2]}'
+    pdf = pdf.drop(range(0, 8))
 
-pdf.iloc[:, 2] = pdf.iloc[:, 2].fillna(' ')
+    pdf = pdf.dropna(axis = 0)
 
-pdf.iloc[:, 1] = pdf.iloc[:, 1] + ' ' + pdf.iloc[:, 2]
+    def formatar_valor(valor):
+        valor = str(valor)
+        valor = valor.replace('(', '').replace(')', '').replace(' ', '').replace('+', '')
+        if valor.endswith('-'):
+            return '-' + valor[:-1]
+        else:
+            return valor
+        
+    pdf.iloc[:, 2] = pdf.iloc[:, 2].apply(formatar_valor)
 
-pdf = pdf.drop(pdf.columns[2], axis=1)
-pdf = pdf.drop(pdf.columns[4], axis=1)
+    pdf['Numero Conta'] = n_conta
 
-pdf = pdf.drop(range(0, 8))
+    pdf = pdf.rename(columns = {pdf.columns[0]: 'Data', pdf.columns[1]: 'Histórico', pdf.columns[2]: 'Valor', pdf.columns[3]: 'Saldo'})
 
-pdf = pdf.dropna(axis = 0)
+    csv_file_path = f'{file.name}.csv'
+    pdf.to_csv(csv_file_path, sep=';', decimal=',', index=False)
 
-def formatar_valor(valor):
-    valor = str(valor)
-    valor = valor.replace('(', '').replace(')', '').replace(' ', '').replace('+', '')
-    if valor.endswith('-'):
-        return '-' + valor[:-1]
-    else:
-        return valor
+    # Download button for the CSV file
+    st.download_button('Baixar arquivo CSV', data=csv_file_path, file_name=f'Carteira.csv')
+
+    # Display DataFrame in DataTable
     
-pdf.iloc[:, 2] = pdf.iloc[:, 2].apply(formatar_valor)
+    st.button('Copiar informações', on_click=pdf.to_clipboard, args=(None,))
 
-pdf['Numero Conta'] = n_conta
-
-download_link = download_excel(pdf)
-st.markdown(download_link, unsafe_allow_html=True)
-
-st.download_button(f'Baixar carteira em CSV', data=download_link, file_name=f'{file}.csv', 
-mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-
-
+    st.dataframe(pdf)
