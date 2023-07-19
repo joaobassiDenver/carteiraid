fe9d760a06e4ece10b2c3ef6a64341c7411fd665
joaobassiDenver <137333862+joaobassiDenver@users.noreply.github.com>
2023-07-19 12:07:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,47 @@
-import streamlit as st
 import pandas as pd
-import tabula
-import io
-import tempfile
-import re
-import base64
-import os
+import streamlit as st
+import yfinance as yf
+from datetime import date
+from io import BytesIO
 
-st.title('Geradora Extrato em CSV')
+st.title('Cotações de Fundos')
 
 st.image('https://th.bing.com/th/id/R.592a9b01eb077958f53aa385dc40f1d5?rik=ESxDzIaUACogwg&pid=ImgRaw&r=0')
 
-file = st.file_uploader(
-    'Importe o extrato em PDF',
-    type='pdf'
+file = st.text_input(
+    'Digite o Fundo desejado'
+    
 )
 
+nome_fundo = f'{file}.SA'
 
-if file is not None:
-    with tempfile.NamedTemporaryFile(suffix='.csv', delete=False) as temp_file:
-        temp_file_path = temp_file.name
-        tabula.convert_into(file, temp_file_path, output_format='csv', pages='all')
+start = st.date_input('Escolha a data inicial: ')
 
-    pdf = pd.read_csv(temp_file_path, encoding='latin1', decimal=',')
+end = st.date_input('Escolha a data final: ')
 
-    conta = pdf.iloc[1][1]
+if start == '' or end == '':
+    st.write('Por favor, escolha as datas desejadas')
+else:
+    cotacao = yf.download(nome_fundo, start=start , end= end, interval='1mo')
 
-    conta = re.findall(r'\d+', conta)
-    n_conta = f'{conta[1]}-{conta[2]}'
+cotacao = cotacao['Close']
 
-    pdf.iloc[:, 2] = pdf.iloc[:, 2].fillna(' ')
+if not cotacao.empty:
+    buffer = BytesIO()
 
-    pdf.iloc[:, 1] = pdf.iloc[:, 1] + ' ' + pdf.iloc[:, 2]
+    with pd.ExcelWriter(buffer, mode='xlsx', engine='openpyxl') as writer:
+            cotacao.to_excel(writer, index=True)
 
-    pdf = pdf.drop(pdf.columns[2], axis=1)
-    pdf = pdf.drop(pdf.columns[4], axis=1)
+    buffer.seek(0)
 
-    pdf = pdf.drop(range(0, 8))
+    def download_arquivo(buffer, nome_arquivo):
+        buffer.seek(0)
+        st.download_button(label='Baixe seu arquivo em xlsx', data=buffer, file_name=nome_arquivo)
 
-    pdf = pdf.dropna(axis = 0)
+    download_arquivo(buffer, f'Cotações_{nome_fundo}.xlsx')
 
-    def formatar_valor(valor):
-        valor = str(valor)
-        valor = valor.replace('(', '').replace(')', '').replace(' ', '').replace('+', '')
-        if valor.endswith('-'):
-            return '-' + valor[:-1]
-        else:
-            return valor
-        
+st.write('Suas cotações: ', cotacao)
 
-    pdf.iloc[:, 2] = pdf.iloc[:, 2].apply(formatar_valor)
-    pdf['Numero Conta'] = n_conta
-    pdf = pdf.rename(columns={pdf.columns[0]: 'Data', pdf.columns[1]: 'Histórico', pdf.columns[2]: 'Valor', pdf.columns[3]: 'Saldo'})
-
-    csv_file_path = 'Extrato.csv'
-    pdf.to_csv(csv_file_path, sep=';', decimal=',', index=False)
-
-    # Função auxiliar para fazer o download do arquivo CSV
-    def download_csv():
-        with open(csv_file_path, 'rb') as file:
-            csv_content = file.read()
-        b64 = base64.b64encode(csv_content).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="Extrato.csv">Baixar arquivo CSV</a>'
-        return href
-
-    # Adicione o botão para download do arquivo CSV
-    st.markdown(download_csv(), unsafe_allow_html=True)
-
-    st.button('Copiar informações', on_click=pdf.to_clipboard, args=(None,))
-
-    st.dataframe(pdf)
 
 
 
